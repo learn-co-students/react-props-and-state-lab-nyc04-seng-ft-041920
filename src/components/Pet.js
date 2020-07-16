aca57292f411edb33238dffdc7844b53ad640738
github username <github email address>
2020-07-15 20:24:31 -0400

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,34 @@
 import React from 'react'
 
 class Pet extends React.Component {
+
   render() {
+    // destructuring
+    const { name, type, age, weight, isAdopted } = this.props.petData
+    const petId = this.props.petData.id
+  
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {isAdopted === true ? '♀ ' : '♂ '}
+            {name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {age}</p>
+            <p>Weight: {weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          
+          <button className={isAdopted === true ? "ui button primary" : "ui button disabled"}>Already adopted</button>
+          {/* if you want pass in an argument for a function inside an onClick event you need to
+          have a callback function '() => {}' otherwise it will invoke that function when the
+          compoent is called. */}
+          <button onClick={() => this.props.onAdoptPet(petId)} className={isAdopted === true ? "ui button disabled" : "ui button primary"}>Adopt pet</button>
         </div>
       </div>
     )
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,17 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+  // destructuring
+  const { myPets } = this.props 
+  return <div className="ui cards">{
+
+    myPets.map(pet => {
+      return <Pet key={pet.id} petData={pet} onAdoptPet={this.props.onAdoptPet} />
+    })
+
+  }</div>
   }
 }
 
